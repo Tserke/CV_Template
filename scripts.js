f7fc0ce4d5a42c58cff673fd915f945cb8e8350c
Tserke <d.Tserkezidis@gmail.com>
2021-07-06 22:27:02 +0300

    cleaned the code, need to add about me

--- scripts.js
+++ scripts.js
@@ -5,28 +5,26 @@ const workForm=document.querySelector("#workForm");
 //function to add two inputs of type text to education form field with a button
 let counterEdu=1
 function addEducationInput(){
+    if (counterEdu<2){
+        const h4 = document.createElement('h4');
+        h4.setAttribute('id','educationh4');
+        h4.innerText="Education";
+        educationForm.append(h4);
+    }
     //date input (type="text")
-    const date = document.createElement('input');
-    date.setAttribute("type","text");
+    const date = document.createElement('p');
+    date.setAttribute("contenteditable","true");
     date.setAttribute("name","eduDate");
     date.setAttribute("id","eduDate"+counterEdu);
-    date.setAttribute("placeholder","date");
+    date.innerText = "date";
     educationForm.append(date);
     //education input (type="text")
-    const input = document.createElement('input');
-    input.setAttribute("type","text");
+    const input = document.createElement('p');
+    input.setAttribute("contenteditable","true");
     input.setAttribute("name","edu"+counterEdu);
     input.setAttribute("id","edu"+counterEdu);
-    input.setAttribute("placeholder","education");
+    input.innerText= "education";
     educationForm.append(input);
-    //br after education date
-    const brDate =document.createElement('br');
-    brDate.setAttribute("id","brEduDate"+counterEdu);
-    date.insertAdjacentElement('afterEnd',brDate);
-    //br after education input
-    const brInput =document.createElement('br');
-    brInput.setAttribute("id","brInput"+counterEdu); 
-    input.insertAdjacentElement('afterEnd',brInput);
     counterEdu++
 }
 //function to remove input of type text from education form with a button input
@@ -41,41 +39,109 @@ function removeEducationInput(){
         let name ="#edu"+counterEdu;
         let inputToBeRemoved= document.querySelector(name);
         inputToBeRemoved.remove();
-        //removes br for date
-        let brDateId="#brEduDate"+counterEdu;
-        let brDateToBeRemoved = document.querySelector(brDateId);
-        brDateToBeRemoved.remove();
-        //removes br for input
-        let brInputId="#brInput"+counterEdu;
-        let brInputToBeRemoved = document.querySelector(brInputId);
-        brInputToBeRemoved.remove();
+    }
+    else{
+        let removeH4 = document.querySelector("#educationh4");
+        removeH4.remove();
+    }
+}
+//function to add language
+let counterLang=1
+function addLanguage(){
+    if (counterLang<2){
+        const h4 = document.createElement('h4');
+        h4.setAttribute('id','language');
+        h4.innerText="Language Skills";
+        educationForm.append(h4);
+    }
+    //date input (type="text")
+    const date = document.createElement('input');
+    date.setAttribute("type","text");
+    date.setAttribute("name","languageName");
+    date.setAttribute("id","languageName"+counterLang);
+    date.innerText = "Language";
+    educationForm.append(date);
+    //education input (type="text")
+    const input = document.createElement('input');
+    input.setAttribute("type","range");
+    input.setAttribute("min","1");
+    input.setAttribute("max","100");
+    input.setAttribute("value","50");
+    input.setAttribute("name","langSlider");
+    input.setAttribute("id","langSlider"+counterLang);
+    educationForm.append(input);
+    counterLang++
+}
+//remove language
+function removeLanguage(){
+    if(counterLang>1){
+        counterLang--;
+        // removes date
+        let languageName ="#languageName"+counterLang;
+        let langToBeRemoved= document.querySelector(languageName);
+        langToBeRemoved.remove();
+        // removes  education
+        let name ="#langSlider"+counterLang;
+        let inputToBeRemoved= document.querySelector(name);
+        inputToBeRemoved.remove();
+    }
+    else{
+        let removeH4 = document.querySelector("#language");
+        removeH4.remove();
+    }
+}
+//function add list
+let counterList=1;
+function addList(){
+    if (counterList<2){
+        const h4 = document.createElement('h4');
+        h4.setAttribute("id","additionalInfo");
+        h4.innerText = "Additional Information";
+        educationForm.append(h4);
+        const ul = document.createElement('ul');
+        ul.setAttribute("id","ul")
+        educationForm.append(ul);
+    }
+    const li = document.createElement('li');
+    li.setAttribute("contenteditable","true");
+    li.setAttribute("id","li"+counterList);
+    ul.append(li);
+    counterList++;
+}
+//function remove list
+function removeList(){
+    if(counterList>1){
+        counterList--;
+        let name ="#li"+counterList;
+        let liToBeRemoved= document.querySelector(name);
+        liToBeRemoved.remove();
+    }
+    else{
+        let removeH4 = document.querySelector("#additionalInfo");
+        removeH4.remove();
+        let ulRemove = document.querySelector("#ul");
+        ulRemove.remove();
     }
 }
 //function to add input of type text to education form field with a button input
 let counterWork=1
 function addWorkInput(){
     //date input (type="text")
-    const date = document.createElement('input');
-    date.setAttribute("type","text");
+    const date = document.createElement('p');
+    date.setAttribute("contenteditable","true");
     date.setAttribute("name","workDate");
     date.setAttribute("id","workDate"+counterWork);
     date.setAttribute("placeholder","date");
+    date.innerText= "date";
     workForm.append(date);
     //work input (type="text")
-    const input = document.createElement('input');
-    input.setAttribute("type","text");
+    const input = document.createElement('p');
+    input.setAttribute("contenteditable","true");
     input.setAttribute("name","work"+counterWork);
     input.setAttribute("id","work"+counterWork);
     input.setAttribute("placeholder","work");
+    input.innerText= "work";
     workForm.append(input);
-    //br after work date
-    const brDate=document.createElement('br');
-    brDate.setAttribute("id","brWorkDate"+counterWork);
-    date.insertAdjacentElement('afterend',brDate);
-    //br after work input
-    const br =document.createElement('br');
-    br.setAttribute("id","brWork"+counterWork)
-    input.insertAdjacentElement('afterend',br)
     counterWork++
 }
 //function to remove input of type text from education form with a button input
@@ -90,13 +156,5 @@ function removeWorkInput(){
         let name ="#work"+counterWork;
         let toBeRemoved= document.querySelector(name);
         toBeRemoved.remove();
-        //removes date br
-        let dateBr = "#brWorkDate"+counterWork;
-        let dateBrToBeRemoved = document.querySelector(dateBr);
-        dateBrToBeRemoved.remove();
-        //removes work br
-        let idName="#brWork"+counterWork;
-        let brToBeRemoved = document.querySelector(idName);
-        brToBeRemoved.remove();
     }
 }
